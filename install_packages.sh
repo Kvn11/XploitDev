224be52789b30b3d4dee60c90996ed8d35962a04
jp3g <kevin.2.romero@uconn.edu>
2024-07-04 18:44:26 -0700

    updates

--- install_packages.sh
+++ install_packages.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 
+USER="jp3g"
+
 # General stuff required
 sudo apt update
 sudo apt install -y \
 python3 python3-pip \
-bc binutils bison dwarves flex gcc git gnupg2 gzip libelf-dev libncurses5-dev libssl-dev make openssl pahole perl-base rsync tar xz-utils \
+bc binutils bison dwarves flex gcc git gnupg2 gzip libelf-dev libncurses5-dev libssl-dev make openssl dwarves perl-base rsync tar xz-utils \
 tmux \
 gdb
 
 # Setup qemu:
 sudo apt update
 sudo apt install qemu qemu-kvm virt-manager bridge-utils
-sudo useradd -g $USER libvirt
-sudo useradd -g $USER libvirt-kvm
-
-sudo useradd -g $USER libvirt
-sudo useradd -g $USER libvirt-kvm
+sudo usermod -aG libvirt $USER
+sudo usermod -aG libvirt-kvm $USER
+sudo usermod -aG libvirt-qemu $USER
