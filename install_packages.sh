70a737ecbcecb5b21f91442e63a21cc0431c8f46
jp3g <kevin.2.romero@uconn.edu>
2024-07-04 19:25:24 -0700

    update dependancies

--- install_packages.sh
+++ install_packages.sh
@@ -5,7 +5,7 @@ USER="jp3g"
 # General stuff required
 sudo apt update
 sudo apt install -y \
-python3 python3-pip \
+python3 python3-pip python3-venv \
 bc binutils bison dwarves flex gcc git gnupg2 gzip libelf-dev libncurses5-dev libssl-dev make openssl dwarves perl-base rsync tar xz-utils \
 tmux \
 gdb
