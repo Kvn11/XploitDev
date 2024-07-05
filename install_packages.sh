c50cf496c6f8e670b7389125b12ece31c244e0e7
jp3g <kevin.2.romero@uconn.edu>
2024-07-04 20:02:26 -0700

    updates

--- install_packages.sh
+++ install_packages.sh
@@ -7,6 +7,7 @@ sudo apt update
 sudo apt install -y \
 python3 python3-pip python3-venv \
 bc binutils bison dwarves flex gcc git gnupg2 gzip libelf-dev libncurses5-dev libssl-dev make openssl dwarves perl-base rsync tar xz-utils \
+libguestfs-tools \
 tmux \
 gdb
 
--- utils/setup_qemu.py
+++ utils/setup_qemu.py
@@ -3,6 +3,7 @@ import os
 from loguru import logger
 import argparse
 from urllib.request import urlretrieve
+import subprocess
 
 DEFAULT_ROOTFS_URL = "https://cloud-images.ubuntu.com/minimal/releases/noble/release-20240618/ubuntu-24.04-minimal-cloudimg-amd64.img"
 
@@ -12,7 +13,7 @@ def download_file(url, directory):
     
     f, headers = urlretrieve(DEFAULT_ROOTFS_URL, file_path)
     logger.info(f"{f} was successfully downloaded.")
-    return f
+    return file_path
     
 def create_directory(directory):
     try:
@@ -27,13 +28,50 @@ def create_directory(directory):
     except OSError as e:
         logger.error(f"Error creating directory {directory}: {e}")
         return False
+    
+def configure_rootfs(rootfs_path, ssh_pub, modules_path, root_pass="root", user_pass="pwn", users=["pwn"], shell="/bin/bash"):
+    
+    # TODO: Add abilityt to read configuration from file
+    
+    command = [
+        "virt-customize",
+        "-a", rootfs_path,
+        "--run-command", "apt-get update",
+        "--install", "ssh-server",
+        "--install", "vim",
+        "--run-command", "systemctl enable ssh",
+        "--root-password", f"password:{root_pass}",
+        "--ssh-inject", f"root:file:{ssh_pub}",
+    ]
+    
+    for usr in users:
+        # Create a new user
+        command.extend([
+            "--run-command", f"useradd -m -s /bin/bash {usr}"
+            ])
+        
+        # TODO: Give ability to set individual passwords.
+        command.extend([
+            "--password", f"usr:password:{user_pass}"
+            ])
+        
+        # Add ssh pub key to user:
+        command.extend([
+            "--ssh-inject", f"{usr}:file:{ssh_pub}"
+        ])
+        
+    command.extend([
+        "--copy-in", f"{modules_path}:/lib/modules"
+    ])
+    
+    result = subprocess.run(command, capture_output=True, text=True)
 
-def main(directory):
+def main(directory, ssh_key, modules_path):
     if not create_directory(directory):
         logger.error("Failed to create or access directory {directory}")
         return
     
-    logger.info("Downloading default cloud image rootfs")
+    logger.info("Downloading default cloud image rootfs, this might take some time.")
     downloaded_file = download_file(DEFAULT_ROOTFS_URL, directory)
     if downloaded_file:
         logger.info("Configuring image...")
@@ -41,13 +79,29 @@ def main(directory):
         logger.error("Failed to set up environment")
         return
     
+    configure_rootfs(
+        rootfs_path=downloaded_file,
+        ssh_pub=ssh_key,
+        modules_path=modules_path,
+        )
+    
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Setup kernel exploitation environment for linux using qemu.")
     
     parser.add_argument("-d", "--dir", type=str, required=True,
                         help="Directory to setup the environment in.")
+    parser.add_argument("-i", "--ssh_key", type=str, required=True,
+                        help="Path to the public ssh key that will be used to login for all users.")
+    parser.add_argument("-m", "--modules", type=str, required=True,
+                        help="Path to the compiled modules you want to use")
     
     args = parser.parse_args()
     
-    main(args.dir)
+    # TODO: Add ability to do download + configure, or just configure
+    
+    main(
+        args.dir,
+        args.ssh_key,
+        args.modules
+        )
